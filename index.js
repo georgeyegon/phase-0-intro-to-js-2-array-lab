3629dd28cde5c596727fa2f92d7beab3eb822a38
George Yegon <georgeyegon67@gmail.com>
2024-09-28 10:25:00 +0300

    Added destructive and non-destructive arrays

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+// Destructive functions
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();  // Remove the last cat from the original array
+}
+
+// Non-destructive functions
+function appendCat(name) {
+    return [...cats, name];  // Return a new array
+}
+
+function prependCat(name) {
+    return [name, ...cats];  // Return a new array
+}
+
+function removeLastCat() {
+    return cats.slice(0, -1);  // Return a new array without the last cat
+}
+
+function removeFirstCat() {
+    return cats.slice(1);  // Return a new array without the first cat
+}
+
+// Exporting the functions
+module.exports = {
+    destructivelyAppendCat,
+    destructivelyPrependCat,
+    destructivelyRemoveFirstCat,
+    destructivelyRemoveLastCat,
+    appendCat,
+    prependCat,
+    removeLastCat,
+    removeFirstCat
+};
